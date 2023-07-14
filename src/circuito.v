ad889f459bfadee9ab6e1475cb10cdfe52907486
NoelFPB <noelpradobucaro@gmail.com>
2023-07-13 22:07:18 -0600

    Update circuito.v

--- src/circuito.v
+++ src/circuito.v
@@ -1,53 +1,12 @@
-module tt_um_chip_SP_NoelFPB(q_out, reset, clk, EN, clk_s, select);
+module tt_um_chip_SP_NoelFPB(q_out, reset, clk, select);
 output [7:0] q_out;
 input reset;
 input clk;
 input [1:0]select;
 reg [11:0] contador;
 reg [7:0] q;
-input EN;
-output clk_s;
-wire W_1;
-wire W_2;
-wire W_3;
-wire W_4;
-wire W_5;
-wire W_6;
-wire W_7;
-wire W_8;
-wire W_9;
-wire W_10;
-wire W_11;
-wire W_12;
-wire W_13;
-wire W_14;
-wire W_15;
-wire W_16;
-wire W_17;
-wire W_18;
-wire W_19;
-wire clk_G;
-assign clk_s = clk_G;
-AND_2 U1(EN,EN,W_1);
-INV  U2(W_1,W_2);
-INV  U3(W_2,W_3);
-INV  U4(W_3,W_4);
-INV  U5(W_4,W_5);
-INV  U6(W_5,W_6);
-INV  U7(W_6,W_7);
-INV  U8(W_7,W_8);
-INV  U9(W_8,W_9);
-INV  U10(W_9,W_10);
-INV  U11(W_10,W_11);
-INV  U12(W_11,W_12);
-INV  U13(W_12,W_13);
-INV  U14(W_13,W_14);
-INV  U15(W_14,W_15);
-INV  U16(W_15,W_16);
-INV  U17(W_16,W_17);
-INV  U18(W_17,W_18);
-INV  U19(W_18,W_19);
-INV  U20(W_19,clk_G);
+
+
 always @ (posedge reset or posedge clk)
 if (reset)
 contador<=12'b000000000000;
@@ -136,13 +95,4 @@ end
 end
 assign q_out = q;
 endmodule
-module INV(A, B);
-input A;
-output B;
-assign B = ~A;
-endmodule
-module AND_2(in1, in2, out);
-input in1, in2;
-output out;
-assign out = in1 & in2;
-endmodule
+
