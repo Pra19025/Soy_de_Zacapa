90ed4f00a43ceaa2b5f73ff0d9ebaef88473bd57
Noel Prado <111771403+NoelFPB@users.noreply.github.com>
2023-08-31 09:39:13 -0600

    Update circuito.v

--- src/circuito.v
+++ src/circuito.v
@@ -1,3 +1,5 @@
+`default_nettype none
+
 module tt_um_chip_SP_NoelFPB(
     input  wire [7:0] ui_in,    // Dedicated inputs - connected to the input switches
     output wire [7:0] uo_out,   // Dedicated outputs - connected to the 7 segment display
