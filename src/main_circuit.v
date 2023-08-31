f7ae384e69b1ad4429ed6b7b634f4d8e993c1f6f
Noel Prado <111771403+NoelFPB@users.noreply.github.com>
2023-08-31 12:24:10 -0600

    Update main_circuit.v

--- src/main_circuit.v
+++ src/main_circuit.v
@@ -12,61 +12,17 @@ module tt_um_chip_SP_NoelFPB(
 );
 
 
+
 wire[1:0]select;
 assign select = ui_in[1:0]; 
-
 reg [11:0] contador;
 reg [7:0] q;
-wire EN;
-wire W_1;
-wire W_2;
-wire W_3;
-wire W_4;
-wire W_5;
-wire W_6;
-wire W_7;
-wire W_8;
-wire W_9;
-wire W_10;
-wire W_11;
-wire W_12;
-wire W_13;
-wire W_14;
-wire W_15;
-wire W_16;
-wire W_17;
-wire W_18;
-wire W_19;
-wire clk_G;
-
 // Assign the input.
-assign uio_out[7:1] = 7'h00;
-assign uio_oe = 8'd1;
-assign EN = ui_in[2];
+assign uio_out = 8'h00;
+assign uo_out = 8'h00;
+assign uio_oe = 8'h00;
 
-assign uio_out[0] = clk_G;
-AND_2 U1(EN,EN,W_1);
-INV  U2(W_1,W_2);
-INV  U3(W_2,W_3);
-INV  U4(W_3,W_4);
-INV  U5(W_4,W_5);
-INV  U6(W_5,W_6);
-INV  U7(W_6,W_7);
-INV  U8(W_7,W_8);
-INV  U9(W_8,W_9);
-INV  U10(W_9,W_10);
-INV  U11(W_10,W_11);
-INV  U12(W_11,W_12);
-INV  U13(W_12,W_13);
-INV  U14(W_13,W_14);
-INV  U15(W_14,W_15);
-INV  U16(W_15,W_16);
-INV  U17(W_16,W_17);
-INV  U18(W_17,W_18);
-INV  U19(W_18,W_19);
-INV  U20(W_19,clk_G);
-
-always @ (posedge clk)
+always @ (posedge rst_n or posedge clk)
 if (rst_n)
 contador<=12'b000000000000;
 else if(select==2'b00 || select==2'b11) begin
