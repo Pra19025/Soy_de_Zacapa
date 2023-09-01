d488cf06d08ce6ca9c1325f70c778ca451817860
Noel Prado <111771403+NoelFPB@users.noreply.github.com>
2023-09-01 11:08:09 -0600

    Update full_anthem.v

--- src/full_anthem.v
+++ src/full_anthem.v
@@ -2,14 +2,15 @@
 
 module tt_um_chip_SP_NoelFPB(
     input  wire [7:0] ui_in,    // Dedicated inputs - connected to the input switches
-    output reg [7:0] uo_out,   // Dedicated outputs - connected to the 7 segment display
-    input  wire [7:0] uio_in,  // IOs: Bidirectional Input path
-    output wire [7:0] uio_out, // IOs: Bidirectional Output path
-    output wire [7:0] uio_oe,  // IOs: Bidirectional Enable path (active high: 0=input, 1=output)
-    input  wire       ena,     // will go high when the design is enabled
-    input  wire       clk,     // clock
-    input  wire       rst_n    // rst_n_n - low to rst_n
+    output wire [7:0] uo_out,   // Dedicated outputs - connected to the 7 segment display
+    input  wire [7:0] uio_in,   // IOs: Bidirectional Input path
+    output wire [7:0] uio_out,  // IOs: Bidirectional Output path
+    output wire [7:0] uio_oe,   // IOs: Bidirectional Enable path (active high: 0=input, 1=output)
+    input  wire       ena,      // will go high when the design is enabled
+    input  wire       clk,      // clock
+    input  wire       rst_n     // rst_n_n - low to rst_n
 );
+
 
     // Assign the input.
     assign uio_out = 8'h00;
