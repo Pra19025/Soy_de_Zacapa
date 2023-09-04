f345c8b0541eebe4e887fdc8f5d564042c9ce6b6
Noel Prado <68123083+Pra19025@users.noreply.github.com>
2023-09-04 10:54:58 -0600

    Update circuito.v

--- src/circuito.v
+++ src/circuito.v
@@ -1,6 +1,6 @@
 `default_nettype none
 
-module tt_um_chip_SP_Soy_de_Zacapa(
+module tt_um_chip_SP_NoelFPB(
     input  wire [7:0] ui_in,    // Dedicated inputs - connected to the input switches
     output wire [7:0] uo_out,   // Dedicated outputs - connected to the 7 segment display
     input  wire [7:0] uio_in,   // IOs: Bidirectional Input path
@@ -22,12 +22,11 @@ reg [7:0] q;
 assign uio_out = 8'h00;
 assign uio_oe = 8'h00;
 
-
-always @ (posedge rst_n or posedge clk)
-if (rst_n)
+always @ (posedge reset or posedge clk)
+if (reset)
 contador<=12'b000000000000;
 else if(select==2'b00 || select==2'b11) begin
-if (contador <58)
+if (contador <125)
 contador <= contador + 1;
 else
 contador <= 0;
@@ -163,7 +162,7 @@ q<=8'b01110011;
 end
 else if(contador == 'd30)
 begin
-q<=8'b00100000;
+q<=8'b00001010;
 end
 else if(contador == 'd31)
 begin
@@ -276,6 +275,274 @@ end
 else if(contador == 'd58)
 begin
 q<=8'b00111011;
+end
+else if(contador == 'd59)
+begin
+q<=8'b00001010;
+end
+else if(contador == 'd60)
+begin
+q<=8'b01101110;
+end
+else if(contador == 'd61)
+begin
+q<=8'b01101001;
+end
+else if(contador == 'd62)
+begin
+q<=8'b00100000;
+end
+else if(contador == 'd63)
+begin
+q<=8'b01101000;
+end
+else if(contador == 'd64)
+begin
+q<=8'b01100001;
+end
+else if(contador == 'd65)
+begin
+q<=8'b01111001;
+end
+else if(contador == 'd66)
+begin
+q<=8'b01100001;
+end
+else if(contador == 'd67)
+begin
+q<=8'b00100000;
+end
+else if(contador == 'd68)
+begin
+q<=8'b01100101;
+end
+else if(contador == 'd69)
+begin
+q<=8'b01110011;
+end
+else if(contador == 'd70)
+begin
+q<=8'b01100011;
+end
+else if(contador == 'd71)
+begin
+q<=8'b01101100;
+end
+else if(contador == 'd72)
+begin
+q<=8'b01100001;
+end
+else if(contador == 'd73)
+begin
+q<=8'b01110110;
+end
+else if(contador == 'd74)
+begin
+q<=8'b01101111;
+end
+else if(contador == 'd75)
+begin
+q<=8'b01110011;
+end
+else if(contador == 'd76)
+begin
+q<=8'b00100000;
+end
+else if(contador == 'd77)
+begin
+q<=8'b01110001;
+end
+else if(contador == 'd78)
+begin
+q<=8'b01110101;
+end
+else if(contador == 'd79)
+begin
+q<=8'b01100101;
+end
+else if(contador == 'd80)
+begin
+q<=8'b00100000;
+end
+else if(contador == 'd81)
+begin
+q<=8'b01101100;
+end
+else if(contador == 'd82)
+begin
+q<=8'b01100001;
+end
+else if(contador == 'd83)
+begin
+q<=8'b01101101;
+end
+else if(contador == 'd84)
+begin
+q<=8'b01100001;
+end
+else if(contador == 'd85)
+begin
+q<=8'b01101110;
+end
+else if(contador == 'd86)
+begin
+q<=8'b00100000;
+end
+else if(contador == 'd87)
+begin
+q<=8'b01100101;
+end
+else if(contador == 'd88)
+begin
+q<=8'b01101100;
+end
+else if(contador == 'd89)
+begin
+q<=8'b00100000;
+end
+else if(contador == 'd90)
+begin
+q<=8'b01111001;
+end
+else if(contador == 'd91)
+begin
+q<=8'b01110101;
+end
+else if(contador == 'd92)
+begin
+q<=8'b01100111;
+end
+else if(contador == 'd93)
+begin
+q<=8'b01101111;
+end
+else if(contador == 'd94)
+begin
+q<=8'b00001010;
+end
+else if(contador == 'd95)
+begin
+q<=8'b01101110;
+end
+else if(contador == 'd96)
+begin
+q<=8'b01101001;
+end
+else if(contador == 'd97)
+begin
+q<=8'b00100000;
+end
+else if(contador == 'd98)
+begin
+q<=8'b01110100;
+end
+else if(contador == 'd99)
+begin
+q<=8'b01101001;
+end
+else if(contador == 'd100)
+begin
+q<=8'b01110010;
+end
+else if(contador == 'd101)
+begin
+q<=8'b01100001;
+end
+else if(contador == 'd102)
+begin
+q<=8'b01101110;
+end
+else if(contador == 'd103)
+begin
+q<=8'b01101111;
+end
+else if(contador == 'd104)
+begin
+q<=8'b01110011;
+end
+else if(contador == 'd105)
+begin
+q<=8'b00100000;
+end
+else if(contador == 'd106)
+begin
+q<=8'b01110001;
+end
+else if(contador == 'd107)
+begin
+q<=8'b01110101;
+end
+else if(contador == 'd108)
+begin
+q<=8'b01100101;
+end
+else if(contador == 'd109)
+begin
+q<=8'b00100000;
+end
+else if(contador == 'd110)
+begin
+q<=8'b01100101;
+end
+else if(contador == 'd111)
+begin
+q<=8'b01110011;
+end
+else if(contador == 'd112)
+begin
+q<=8'b01100011;
+end
+else if(contador == 'd113)
+begin
+q<=8'b01110101;
+end
+else if(contador == 'd114)
+begin
+q<=8'b01110000;
+end
+else if(contador == 'd115)
+begin
+q<=8'b01100001;
+end
+else if(contador == 'd116)
+begin
+q<=8'b01101110;
+end
+else if(contador == 'd117)
+begin
+q<=8'b00100000;
+end
+else if(contador == 'd118)
+begin
+q<=8'b01110100;
+end
+else if(contador == 'd119)
+begin
+q<=8'b01110101;
+end
+else if(contador == 'd120)
+begin
+q<=8'b00100000;
+end
+else if(contador == 'd121)
+begin
+q<=8'b01100110;
+end
+else if(contador == 'd122)
+begin
+q<=8'b01100001;
+end
+else if(contador == 'd123)
+begin
+q<=8'b01111010;
+end
+else if(contador == 'd124)
+begin
+q<=8'b00101110;
+end
+else if(contador == 'd125)
+begin
+q<=8'b00001010;
 end
 end
 else if(select==2'b01 || select==2'b10)
